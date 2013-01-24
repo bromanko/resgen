bd61fd07b4dbd499758ed00be70d0f98bdfe7a82
Brian Romanko <brian@bigbigbomb.com>
2013-01-24 08:42:50 -0600

    Testing output of classes for each folder

--- build.py
+++ build.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
 import os
-import re
-import subprocess
 import logging
 import optparse
 
@@ -27,13 +25,42 @@ def main():
     if not exists:
         parser.error("The resource folder specified (%s) does not exist." % options.resource_folder)
 
-    r_file_path = os.path.join(options.resource_folder, options.class_name + '.h')
-    logging.info('Creating "%s" resources registry class', r_file_path)
-    r_file = open(r_file_path, 'w')
+    rFilePath = os.path.join(options.resource_folder, options.class_name + '.h')
+    logging.info('Creating "%s" resources registry class', rFilePath)
+    rFile = open(rFilePath, 'w')
+
+    write_header(rFile)
+
+    for (dirPath, dirNames, fileNames) in os.walk(options.resource_folder):
+        logging.info("%s %s %s" % (dirPath, dirNames, fileNames))
+        write_class(options.class_name, rFile, dirNames, fileNames)
+        # For each folder we will need to emit a class
+        # For each file in the folder we will need to emit properties on that class
 
 
-    r_file.write("hi there 2")
-    r_file.close()
+    rFile.close()
+
+def write_header(file):
+    file.write("Header\n")
+
+def write_class(rClassName, file, className, properties):
+    className = "__%s_%s" % (rClassName, className)
+    file.write('@interface %s : NSObject\n' % className)
+    file.write('\n')
+    for (p) in properties:
+        file.write('- (NSString *)%s;\n' % p)
+    file.write('\n')
+    file.write('@end\n')
+    file.write('\n')
+    file.write('@implementation %s\n' % className)
+    file.write('\n')
+    for (p) in properties:
+        file.write('- (NSString *)%s {\n' % p)
+        file.write('return @"%s";\n' % p)
+    file.write('}\n')
+    file.write('\n')
+    file.write('@end\n')
+    file.write('\n')
 
 
 if __name__ == "__main__":
