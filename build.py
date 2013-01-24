dd9932601bc12b0a3906bd05937f4fb76bcd6420
Brian Romanko <brian@bigbigbomb.com>
2013-01-24 15:08:44 -0600

    Misunderstanding of how os.walk works. Need to remove the recursion

--- build.py
+++ build.py
@@ -9,7 +9,44 @@ import re
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(message)s')
 
-options = ''
+class Property:
+    def __init__(self, name, fileName):
+        self.name = name
+        self.fileName = fileName
+
+class Node:
+    def __init__(self, name):
+        self.name = name
+        self.parent = None
+        self.nodes = []
+        self.properties = []
+
+    def full_name(self):
+        full = self.name
+        parent = self.parent
+        while parent is not None:
+            full += "_" + parent.name
+            parent = parent.parent
+        return full
+
+    def write_class(self, file):
+        file.write('@interface %s : NSObject\n' % self.full_name())
+        file.write('\n')
+        for (p) in self.properties:
+            file.write('- (NSString *)%s;\n' % p.name)
+        file.write('\n')
+        file.write('@end\n')
+        file.write('\n')
+#        file.write('@implementation %s\n' % className)
+#        file.write('\n')
+#        for (p) in fileNames:
+#            file.write('- (NSString *)%s {\n' % os.path.splitext(p)[0])
+#            file.write('return @"%s";\n' % p)
+#        file.write('}\n')
+#        file.write('\n')
+#        file.write('@end\n')
+#        file.write('\n')
+
 
 def main():
     usage = "usage: %prog [options]"
@@ -29,52 +66,36 @@ def main():
     if not exists:
         parser.error("The resource folder specified (%s) does not exist." % options.resource_folder)
 
+    root = Node(options.class_name)
+    walk_folder(root, options.resource_folder)
+
     rFilePath = os.path.join(options.resource_folder, options.class_name + '.h')
     logging.info('Creating "%s" resources registry class', rFilePath)
     rFile = open(rFilePath, 'w')
 
     write_header(rFile)
-    walk_folder(options.resource_folder, options, rFile)
+    root.write_class(rFile)
     rFile.close()
 
-def walk_folder(folder, options, file):
+def walk_folder(node, folder):
     includes = ['*.png', '*.jpg', '*.jpeg', '*.gif', '*.ttf', '*.otf', '*.mp3', '*.caf']
     includes = r'|'.join([fnmatch.translate(x) for x in includes])
 
     for (dirPath, dirNames, fileNames) in os.walk(folder):
         logging.debug("Processing folder %s %s %s" % (dirPath, dirNames, fileNames))
-        if dirPath != options.resource_folder:
-            filtered = [f for f in fileNames if re.match(includes, f)]
-            write_class(options, os.path.split(dirPath)[1], filtered, file)
+        filtered = [f for f in fileNames if re.match(includes, f)]
+        for f in filtered:
+            node.properties.append(Property(os.path.splitext(f)[0], f))
 
-        for (dir) in dirNames:
-            walk_folder(dir, options, file)
-
-        # For each file in the folder we will need to emit properties on that class
+#        for (dir) in dirNames:
+#            logging.debug("Creating class %s" % dir)
+#            child = Node(dir)
+#            node.nodes.append(child)
+#            child.parent = node
+#            walk_folder(child, dir)
 
 def write_header(file):
     file.write("Header\n")
-
-def write_class(options, className, fileNames, file):
-    logging.debug("Creating class %s" % className)
-
-    className = "__%s_%s" % (options.class_name, className)
-    file.write('@interface %s : NSObject\n' % className)
-    file.write('\n')
-    for (p) in fileNames:
-        file.write('- (NSString *)%s;\n' % os.path.splitext(p)[0])
-    file.write('\n')
-    file.write('@end\n')
-    file.write('\n')
-    file.write('@implementation %s\n' % className)
-    file.write('\n')
-    for (p) in fileNames:
-        file.write('- (NSString *)%s {\n' % os.path.splitext(p)[0])
-        file.write('return @"%s";\n' % p)
-    file.write('}\n')
-    file.write('\n')
-    file.write('@end\n')
-    file.write('\n')
 
 
 if __name__ == "__main__":
